52049659fa19d105ac176f9dd102a15a739b8f25
XinboAndroid <suiyc@xinboedu.com>
2013-12-03 03:25:26 -0800

    改错   by ouzhengjian

--- unknown/src/com/example/ex_templete/LaiguangqiangActivity9.java
+++ unknown/src/com/example/ex_templete/LaiguangqiangActivity9.java
@@ -6,22 +6,24 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class LaiguangqiangActivity9 extends Activity {
-
-	public void btu(View v) {
-		startActivity(new Intent(this,LiaoHuiActivity10.class));
-		
+public class LaiguangqiangActivity9 extends Activity
+{
+	public void btu(View v)
+	{
+		startActivity(new Intent(this, LiaoHuiActivity10.class));
 	}
-	protected void onCreate(Bundle savedInstanceState) {
+
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_laiguangqiang_activity9);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.laiguangqiang_activity9, menu);
+		getMenuInflater().inflate(R.menu.ruan_activity10, menu);
 		return true;
 	}
-
 }
