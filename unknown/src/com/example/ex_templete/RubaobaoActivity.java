a9b2b351865a37548a15bc81779766fd70c4edb3
XinboAndroid <suiyc@xinboedu.com>
2014-02-23 04:28:10 -0800

    animation wuya 李豫

--- unknown/src/com/example/ex_templete/RubaobaoActivity.java
+++ unknown/src/com/example/ex_templete/RubaobaoActivity.java
@@ -3,7 +3,10 @@ package com.example.ex_templete;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.view.Menu;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class RubaobaoActivity extends Activity
 {
@@ -13,7 +16,33 @@ public class RubaobaoActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_rubaobao);
-		startActivity(new Intent(this, ShankeActivity.class));
+		final ImageView imgWuyaView=(ImageView) findViewById(R.id.img_wuya);
+		new Thread(new Runnable()
+		{
+			
+			@Override
+			public void run()
+			{
+				try
+				{
+					Thread.sleep(500);
+					AnimationDrawable frameAnim = (AnimationDrawable) imgWuyaView.getDrawable();
+					frameAnim.stop();
+					frameAnim.start();
+					Thread.sleep(6000);
+					startActivity(new Intent(RubaobaoActivity.this, ShankeActivity.class));
+				}
+				catch (InterruptedException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}).start();
+		imgWuyaView.startAnimation(
+				AnimationUtils.loadAnimation(RubaobaoActivity.this, 
+						R.anim.wuya_set));
 	}
 
 	@Override
