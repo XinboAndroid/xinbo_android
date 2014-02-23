b89d690e2298915122974581f8075187ad9b2fb4
XinboAndroid <suiyc@xinboedu.com>
2014-02-23 09:19:27 -0800

    huangpn bgmusic

--- unknown/src/com/example/ex_templete/RubaobaoActivity.java
+++ unknown/src/com/example/ex_templete/RubaobaoActivity.java
@@ -1,7 +1,14 @@
 package com.example.ex_templete;
 
+import java.io.FileDescriptor;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.graphics.drawable.AnimationDrawable;
 import android.text.TextPaint;
@@ -14,16 +21,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RubaobaoActivity extends Activity
-{
+public class RubaobaoActivity extends Activity {
 	
+	private MediaPlayer mMediaPlayer;
 	private EditText mEditText1;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_rubaobao);
+		
 		TextView textView1 = (TextView) findViewById(R.id.textView1);
 		TextView textView2 = (TextView) findViewById(R.id.textView2);
 		mEditText1 = (EditText) findViewById(R.id.editText1);
@@ -32,52 +39,75 @@ public class RubaobaoActivity extends Activity
 		tp1.setFakeBoldText(true);
 		tp2.setFakeBoldText(true);
 
-		final ImageView imgWuyaView=(ImageView) findViewById(R.id.img_wuya);
-		new Thread(new Runnable()
-		{
-			
+		final ImageView imgWuyaView = (ImageView) findViewById(R.id.img_wuya);
+		new Thread(new Runnable() {
+
 			@Override
-			public void run()
-			{
-				try
-				{
+			public void run() {
+				try {
 					Thread.sleep(500);
-					AnimationDrawable frameAnim = (AnimationDrawable) imgWuyaView.getDrawable();
+					AnimationDrawable frameAnim = (AnimationDrawable) imgWuyaView
+							.getDrawable();
 					frameAnim.stop();
 					frameAnim.start();
 					Thread.sleep(6000);
-				}
-				catch (InterruptedException e)
-				{
+				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
 		}).start();
-		imgWuyaView.startAnimation(
-				AnimationUtils.loadAnimation(RubaobaoActivity.this, 
-						R.anim.wuya_set));
+		imgWuyaView.startAnimation(AnimationUtils.loadAnimation(
+				RubaobaoActivity.this, R.anim.wuya_set));
 	}
 
-			
-		public void next(View v) {
+	public void next(View v) {
 
-		if("1311".equals(mEditText1.getText().toString())){
-			startActivity(new Intent(RubaobaoActivity.this, ShankeActivity.class));
-		}else{
-			Toast.makeText(RubaobaoActivity.this, "笨蛋，输错啦！", Toast.LENGTH_LONG).show();
+		if ("1311".equals(mEditText1.getText().toString())) {
+			startActivity(new Intent(RubaobaoActivity.this,
+					ShankeActivity.class));
+		} else {
+			Toast.makeText(RubaobaoActivity.this, "笨蛋，输错啦！", Toast.LENGTH_LONG)
+					.show();
 		}
 
-		
 	}
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	protected void onResume() {
+		super.onResume();
+		initMusicbg();
+	}
+	
+	private void initMusicbg() {
+
+		try {
+			AssetManager am = getAssets();
+			AssetFileDescriptor afd = am.openFd("hd.mp3");
+			FileDescriptor fd = afd.getFileDescriptor();
+			mMediaPlayer = new MediaPlayer();
+			mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+			mMediaPlayer.setDataSource(fd, afd.getStartOffset(),
+					afd.getLength());
+			mMediaPlayer.prepare();
+			mMediaPlayer.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.rubaobao, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mMediaPlayer.stop();
 	}
 
 }
