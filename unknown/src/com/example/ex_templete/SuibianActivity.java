efca3ddf1843ea50ba376b209e9591fddcca3a46
XinboAndroid <suiyc@xinboedu.com>
2014-04-12 10:14:38 +0800

    by hemeiwu  2014-04-12 10:13

---
+++ unknown/src/com/example/ex_templete/SuibianActivity.java
@@ -0,0 +1,40 @@
+package com.example.ex_templete;
+//hemeiwu  2014-04-12 10:12
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SuibianActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_suibian);
+		ImageView image = (ImageView) findViewById(R.id.suibian_imageView1);
+		Animation animation = AnimationUtils.loadAnimation(this, R.anim.suibian_animation);
+		image.startAnimation(animation);
+		
+		findViewById(R.id.suibian_button1).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent=new Intent();
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.suibian, menu);
+		return true;
+	}
+
+}
