1bf0788c0071ee70e97fbb8f3efe16917aaf8120
XinboAndroid <suiyc@xinboedu.com>
2014-04-14 21:27:51 +0800

    修改了HeheActivity,实现跳转和页面效果 by wyx 2014-04-14 21:30

--- unknown/src/com/example/ex_templete/HeheActivity.java
+++ unknown/src/com/example/ex_templete/HeheActivity.java
@@ -2,11 +2,62 @@ package com.example.ex_templete;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class HeheActivity extends Activity
 {
+    private boolean isGoNext;
 
+    /**
+     * @wyx
+     * 2014-0414 
+     */
+    public void btnShowSurprise(View v)
+    {
+        //跳转下一个页面
+        if(isGoNext)
+        {
+            View layout = findViewById(R.id.retive_view);
+            layout.setBackgroundResource(R.drawable.ppmm2);
+            Toast.makeText(HeheActivity.this, "哈哈，没有骗你吧。。。", Toast.LENGTH_LONG).show();
+            v.postDelayed(new Runnable()
+            {
+                
+                @Override
+                public void run()
+                {
+                    //跳转到下一个页面接口；
+                    Intent intent = null;
+                    //跳转可以修改HahaActivity类；
+                    intent = new Intent(HeheActivity.this,HahaActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 2000);
+        }
+        if(!isGoNext)
+        {
+            new AlertDialog.Builder(this).setTitle("")
+            .setMessage("Unfortunately,SimplePuzzle    \nhas stopped.")
+            .setNegativeButton("OK", new DialogInterface.OnClickListener()
+            {
+                
+                @Override
+                public void onClick(DialogInterface dialog, int which)
+                {
+                    isGoNext = true;
+                    Toast.makeText(HeheActivity.this, "再点，还会有惊奇的哦", Toast.LENGTH_LONG).show();
+                }
+            })
+            .show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
