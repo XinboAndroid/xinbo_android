7ce516b5f645e21847599bb1e61a5c07d1100c6d
suiyuchen <suiyc@xinboedu.com>
2014-10-22 17:24:04 +0800

    A

--- unknown/src/com/example/_xinbo09/HuChunJieActivity.java
+++ unknown/src/com/example/_xinbo09/HuChunJieActivity.java
@@ -7,34 +7,33 @@ import android.view.Menu;
 import android.view.View;
 
 public class HuChunJieActivity extends Activity {
-	
+
 	/**
 	 * sadfasdf
+	 * 
 	 * @param v
 	 */
 	public void xinbo_btn(View v) {
-	Intent intent=new Intent();
-//		startActivity(intent);
-//		finish();                   
-        int a;
-        int b;
-        int c;
+		Intent intent = new Intent();
+		startActivity(intent);
+		finish();
+		int a;
+		int b;
+		int c;
 	}
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	//û�в����ļ�
-	//	setContentView(R.layout.activity_hu_chun_jie);
-		
-		
+		// û�в����ļ�
+		// setContentView(R.layout.activity_hu_chun_jie);
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-	//	getMenuInflater().inflate(R.menu.hu_chun_jie, menu);
+		// getMenuInflater().inflate(R.menu.hu_chun_jie, menu);
 		return true;
 	}
 
