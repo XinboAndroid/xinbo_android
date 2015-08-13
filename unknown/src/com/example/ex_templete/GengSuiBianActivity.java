5f643b55fff98f6a4df7fa060db94381a120d687
suiyuchen <suiyc@xinboedu.com>
2015-08-13 16:01:26 +0800

    xxx修改了xxxbug

--- unknown/src/com/example/ex_templete/GengSuiBianActivity.java
+++ unknown/src/com/example/ex_templete/GengSuiBianActivity.java
@@ -8,28 +8,25 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class GengSuiBianActivity extends Activity
-{
+public class GengSuiBianActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		//4444444
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_geng_sui_bian);
+		// 2015-08-13 修改
 	}
-//东东
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.geng_sui_bian, menu);
 		return true;
 	}
-	public void btn(View v)
-	{
+
+	public void btn(View v) {
 		// TODO Auto-generated method stub
-		Intent intent=new Intent(this,Xinbo09Activity.class);
+		Intent intent = new Intent(this, Xinbo09Activity.class);
 		startActivity(intent);
 	}
 }
